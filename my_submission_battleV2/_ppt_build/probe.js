eba326177b258d57f6e5f3cc7d7ae069fed34bf0
dae-hany <daehoney9889@gmail.com>
2026-06-02 09:48:19 +0900

    cache 삭제

--- my_submission_battleV2/_ppt_build/probe.js
+++
@@ -1,7 +0,0 @@
-const sharp = require("sharp");
-const svg = `<svg width="600" height="160" xmlns="http://www.w3.org/2000/svg">
-<rect width="600" height="160" fill="#ffffff"/>
-<text x="20" y="60" font-family="Inter, 'Malgun Gothic', sans-serif" font-size="28" fill="#4a154b">한글 테스트 DaehoV2 배틀</text>
-<text x="20" y="110" font-family="'Malgun Gothic', sans-serif" font-size="24" fill="#1d1d1d">팀빌딩 선발 전략 감지</text>
-</svg>`;
-sharp(Buffer.from(svg)).png().toFile("probe.png").then(()=>console.log("ok")).catch(e=>{console.error(e);process.exit(1);});
